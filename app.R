48e9440893cd038adb31cf52c7f8c9734eb50e27
timolingh <timolin@gmail.com>
2018-09-18 21:05:03 -0700

    New project

---
+++ app.R
@@ -0,0 +1,170 @@
+library(data.table)
+library(ggplot2)
+library(shiny)
+library(DT)
+library(scales)
+
+## Simulation functions
+normSim <- function(mu, s, nyear, nsim) {
+  
+  ## The size of the matrix
+  N = nyear * nsim
+  
+  ## Generate returns
+  r = rnorm(n=N, mean=mu, sd=s)
+  
+  ## organize as a nyear x nsim matrix
+  R <- matrix((1 + r), nrow=nyear)
+  
+  ## The cumulative returns over the specified horizon
+  #returns <- apply(R, 2, prod)
+  
+}
+
+# lnormSim <- function(mu, s, nyear, nsim) {
+#   
+#   ## The size of the matrix
+#   N = nyear * nsim
+#   
+#   ## Generate returns
+#   r = rlnorm(n=N, meanlog=mu, sdlog=s)
+#   
+#   ## organize as a nyear x nsim matrix
+#   R <- matrix(log(r), nrow=nyear)
+#   
+#   ## The cumulative returns over the specified horizon
+#   returns <- apply(R, 2, sum)
+#   
+#   ## The distribution above is log(returns)
+#   returns <- exp(returns)
+#   
+# }
+
+
+## UI
+ui <- fluidPage(
+  titlePanel("Simple portfolio simulation"),
+  sidebarLayout(
+    sidebarPanel(
+      p("Model inputs"),
+      numericInput("seed_value", "Seed value:", 8888),
+      numericInput("initial_investment", "Initial investment:", 1000),
+      numericInput("nsim", "Number of simulations to run:", 500),
+      numericInput("nyear", "Investment horizon in years:", 20),
+      numericInput("mupct", "Expected return (%):", 5),
+      numericInput("sdpct", "Portfolio std. dev. (%):", 20),
+      br(),
+      actionButton("goButton", "Submit"),
+      p("Click button to start simulation")
+    ),
+    
+    mainPanel(
+      h4("Simulation results - value of investment at the end of horizon"),
+      tableOutput("sim_result"),
+      
+      h4("Distribution of outcomes"),
+      plotOutput("simulation_outcomes"),
+      
+      h4("Simulation traces - growth of investment over time"),
+      plotOutput("plot_sims")
+      
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  # resM <- reactive({
+  #   set.seed(8888)
+  #   res_matrix <- normSim(input$mupct / 100, input$sdpct / 100, input$nyear, input$nsim)
+  #   
+  # })
+  
+  seed_value <- eventReactive(input$goButton, {
+    input$seed_value
+  })
+  
+  init_investment <- eventReactive(input$goButton, {
+    input$initial_investment
+  })
+  
+  nyear <- eventReactive(input$goButton, {
+    input$nyear
+  })
+  
+  nsim <- eventReactive(input$goButton, {
+    input$nsim
+  })
+  
+  resM <- eventReactive(input$goButton, {
+    set.seed(seed_value())
+    res_matrix <- normSim(input$mupct / 100, input$sdpct / 100, input$nyear, input$nsim)
+    
+  })
+  
+  res <- reactive({
+    res_vector <- apply(resM(), 2, prod) * init_investment()
+  })
+  
+  sim_table <- reactive({
+    M <- resM()
+    cumM <- apply(M, 2, cumprod)
+    dt <- data.table(cumM)
+    
+  })
+  
+  output$sim_result <- renderTable({
+    q <- c(0.2, 0.5, 0.8)
+    quantile_dt <- data.table(Quantile = q, Value = quantile(res(), q))
+  })
+  
+  output$sim_table <- renderTable({
+    sim_table()
+  })
+  
+  output$plot_sims <- renderPlot({
+    dt <- sim_table()
+    dt[, yr := 1:nyear()]
+    dt_long <- melt(dt, id.vars = "yr", variable.name = "sim_id", value.name = "cum_return")
+    dt_long[, portfolio_value := cum_return * init_investment()]
+    pl <- ggplot(dt_long) + geom_line(aes(as.factor(yr), portfolio_value, group=sim_id, color=sim_id)) +
+      xlab("Year") + ylab("Portfolio value") + scale_y_continuous(label=dollar_format()) +
+      guides(color=F)
+    pl
+    
+  })
+  
+  output$simulation_outcomes <- renderPlot({
+    sim_index <- 1:nsim()
+    ending_value <- res()
+    dt <- data.table(sim_index, ending_value)
+    
+    pl <- ggplot(dt) + stat_density(aes(x=ending_value), color="blue", geom="line") +
+      xlab("Ending value") + ylab("Density") + scale_x_continuous(label=dollar_format())
+    pl
+    
+  })
+  
+  
+}
+
+shinyApp(ui, server)
+
+
+# # Return a vector of cum returns over the number of simulations
+# normResult <- normSim(mu, s, nyear, nsim)
+# logNormResult <- lnormSim(mu, s, nyear, nsim)
+# 
+# dt <- data.table(normRet = normResult, logNormRet = logNormResult)
+# 
+# # print(quantile(normResult, 0.2))
+# # print(quantile(logNormResult, 0.2))
+# 
+# pl <- ggplot(dt) + geom_density(aes(x=normResult), color="blue") + 
+#   geom_density(aes(x=logNormResult), color="red") 
+# 
+# print(pl)
+# 
+# niter <- 1000:5000
+# conv <- sapply(niter, function(x) quantile(normSim(mu, s, nyear, x), 0.5))
+# plot(niter, conv)
